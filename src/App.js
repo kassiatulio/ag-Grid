2950b3844aea93ec8fd6daf0403fdb466da2fc6e
kassiatulio <kassia.tulio@gmail.com>
2019-06-11 23:37:58 +0100

    List of user by api.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    
+    this.state = {
+      columnDefs: [{
+        headerName: "ID", field: "id", filter: true
+      }, {
+        headerName: "Nome", field: "first_name", filter: true
+      }, {
+        headerName: "Apelido", field: "last_name", filter: true
+      }, {
+        headerName: "Gênero", field: "gender", filter: true  
+      }],
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://gorest.co.in/public-api/users', { headers: { 'Authorization': 'Bearer SGu_9ggmXj5K0cH1UwN-rK0eA6cI_hWrzjZ2' } })
+      .then(response => {
+        this.setState({ rowData: response.data.result })
+      })
+    }
+
+  render() {
+    return (
+      <div>
+        <h1>Usuários</h1>
+        <div 
+          className="ag-grid ag-theme-balham"
+          style={{ 
+          height: '500px', 
+          width: '100%' }} 
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <AgGridReact
+            columnDefs={this.state.columnDefs}
+            rowData={this.state.rowData}>
+          </AgGridReact>      
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
